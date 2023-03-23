97cca03615ec299163b8035855497b70f2ca67d4
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-03-23 14:13:15 +0400

    feature:fetch api was successfully tested

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
 const getPhotos = document.getElementById("btn");
-getPhotos.addEventListener("click",addConsole);
 
-function addConsole(params) {
-    console.log("Clicked");
-    
+getPhotos.addEventListener("click",fetchImage)
+
+function fetchImage(params) {
+ const inputValue = document.getElementById("input").value
+fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=${Math.round(Math.random()*1000)}&client_id=GV2146OATxwIUAjpTpKgvGjAM_UI-5Iw--7Cag2886w`).then((res)=>res.json().then((data)=>{console.log(data);}))
+  
 }
