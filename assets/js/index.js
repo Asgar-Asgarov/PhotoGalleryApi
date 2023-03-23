fc602816993a6754f927ea629112809fb1e668ec
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-03-23 15:27:10 +0400

    refactor:try catch was added for reflecting error to screen

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,17 +2,31 @@ const getPhotos = document.getElementById("btn");
 const errorMessage = document.getElementById("errorMessage");
 const input = document.getElementById("input");
 
-
-getPhotos.addEventListener("click",fetchImage)
+getPhotos.addEventListener("click", fetchImage);
 
 async function fetchImage(params) {
- const inputValue = input.value;
- if (inputValue<0||inputValue>10) {    
-    errorMessage.style.display="block";
-    errorMessage.innerText = "Number should be between 0 and 11";    
+  const inputValue = input.value;
+  if (inputValue < 0 || inputValue > 10) {
+    errorMessage.style.display = "block";
+    errorMessage.innerText = "Number should be between 0 and 11";
     return;
-}
-await fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=${Math.round(Math.random()*1000)}&client_id=GV2146OATxwIUAjpTpKgvGjAM_UI-5Iw--7Cag2886w`).then((res)=>res.json().then((data)=>{console.log(data);}))
+  }
+  try { 
+     await fetch(
+    `https://api.unsplash.com/photos?per_page=${inputValue}&page=${Math.round(
+      Math.random() * 1000
+    )}&client_id=GV2146OATxwIUAjpTpKgvGjAM_UI-5Iw--7Cag2886w`
+  ).then((res) =>
+    res.json().then((data) => {
+      console.log(data);
+    })
+  );
 
-errorMessage.style.display="none";
+  errorMessage.style.display = "none";
+    
+  } catch (error) {
+    errorMessage.style.display = "block";
+    errorMessage.innerText = "An error occured , please  try again later";    
+  }
+
 }
